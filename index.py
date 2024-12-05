ddfb2b1263ed2c41f9e795d2fa1f2204dbba4108
Schmidt <118033315+Schmidt1999@users.noreply.github.com>
2024-12-04 22:12:37 -0300

    initial code

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+lifeExpectancy = 73
+age = int(input("Insira sua idade: "))
+
+progress = age / lifeExpectancy
+
+def progress():
+
+
+if age.isinstance(str):
+    print("Por favor insira um número")
+if age > lifeExpectancy:
+    print("Você ultrapassou a expectativa de vida brasileira!")
+elif age.isinstanc(int):
+    print(f"Você progrediu {progress}% de sua vida!")
